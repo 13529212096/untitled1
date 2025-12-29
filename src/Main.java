5dfaa90bfbc7e0b44bc449ec44b1dec668dbab7e
Unknown <1524692082@qq.com>
2025-12-29 11:29:39 +0800

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("欢迎来到猜数字小游戏！");
+        System.out.println("我已经想好了一个1到100之间的数字，你能猜到它吗？");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Random random = new Random();
+        int answer = random.nextInt(100) + 1;
+        int guess;
+        int attempts = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("请输入你的猜测（1-100）：");
+            if (!scanner.hasNextInt()) {
+                System.out.println("请输入一个有效的数字！");
+                scanner.next(); // 清除无效输入
+                continue;
+            }
+            guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < 1 || guess > 100) {
+                System.out.println("数字范围是1到100，请重新输入！");
+            } else if (guess < answer) {
+                System.out.println("太小了，再试试！");
+            } else if (guess > answer) {
+                System.out.println("太大了，再试试！");
+            } else {
+                System.out.println("恭喜你，猜对了！答案就是 " + answer + "。");
+                System.out.println("你一共猜了 " + attempts + " 次。");
+                break;
+            }
         }
+
+        System.out.println("游戏结束，感谢游玩！");
     }
 }
